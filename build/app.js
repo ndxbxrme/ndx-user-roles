0ee40f60c66370e07eb01be13994a8036cecc9d5
ndxbxrme <lewis_the_cat@hotmail.com>
2017-02-04 11:10:47 +0000

    check for user

--- build/app.js
+++ build/app.js
@@ -1,117 +1,116 @@
 (function() {
   'use strict';
   module.exports = function(ndx) {
-    ndx.app.use('/api/*', function(req, res, next) {
-      if (!req.user) {
-        req.user = {};
-      }
-      if (Object.prototype.toString.call(req.user) === '[object Object]') {
-        req.user.addRole = function(role) {
-          var addKey, j, key, keys, len, root;
-          addKey = function(root, key) {
-            if (!root[key]) {
-              root[key] = {};
+    return ndx.app.use('/api/*', function(req, res, next) {
+      if (req.user) {
+        if (Object.prototype.toString.call(req.user) === '[object Object]') {
+          req.user.addRole = function(role) {
+            var addKey, j, key, keys, len, root;
+            addKey = function(root, key) {
+              if (!root[key]) {
+                root[key] = {};
+              }
+              return root[key];
+            };
+            keys = role.split(/\./g);
+            if (!req.user.roles) {
+              req.user.roles = {};
             }
-            return root[key];
-          };
-          keys = role.split(/\./g);
-          if (!req.user.roles) {
-            req.user.roles = {};
-          }
-          root = req.user.roles;
-          for (j = 0, len = keys.length; j < len; j++) {
-            key = keys[j];
-            root = addKey(root, key);
-          }
-          return ndx.database.exec('UPDATE ' + ndx.settings.USER_TABLE + ' SET roles=? where _id=?', [req.user.roles, req.user._id]);
-        };
-        req.user.removeRole = function(role) {
-          var getKey, i, key, keys, root;
-          getKey = function(root, key) {
-            return root[key];
-          };
-          keys = role.split(/\./g);
-          if (req.user.roles && keys.length) {
-            root = req.user.roles;
-            i = 0;
-            key = '';
-            while (i < keys.length - 1) {
-              key = keys[i];
-              root = getKey(root, key);
-              i++;
-            }
-            key = keys[keys.length - 1];
-            if (root[key]) {
-              delete root[key];
-            }
-          }
-          return ndx.database.exec('UPDATE ' + ndx.settings.USER_TABLE + ' SET roles=? where _id=?', [req.user.roles, req.user._id]);
-        };
-        req.user.hasRole = function(role) {
-          var allgood, getKey, j, key, keys, len, root;
-          getKey = function(root, key) {
-            return root[key];
-          };
-          keys = role.split(/\./g);
-          allgood = false;
-          if (req.user.roles) {
             root = req.user.roles;
             for (j = 0, len = keys.length; j < len; j++) {
               key = keys[j];
-              root = getKey(root, key);
-              if (root) {
-                allgood = true;
-              } else {
-                allgood = false;
-                break;
-              }
+              root = addKey(root, key);
             }
-          }
-          return allgood;
-        };
-      }
-      return next();
-    });
-    return ndx.authenticate = function(role) {
-      return function(req, res, next) {
-        var getRole, j, len, r, rolesToCheck, truth;
-        if (req.user) {
-          rolesToCheck = [];
-          getRole = function(role) {
-            var j, len, r, results, type;
-            type = Object.prototype.toString.call(role);
-            if (type === '[object Array]') {
-              results = [];
-              for (j = 0, len = role.length; j < len; j++) {
-                r = role[j];
-                results.push(getRole(r));
-              }
-              return results;
-            } else if (type === '[object Function]') {
-              r = role(req);
-              return getRole(r);
-            } else if (type === '[object String]') {
-              if (rolesToCheck.indexOf(role) === -1) {
-                return rolesToCheck.push(role);
-              }
-            }
+            return ndx.database.exec('UPDATE ' + ndx.settings.USER_TABLE + ' SET roles=? where _id=?', [req.user.roles, req.user._id]);
           };
-          getRole(role);
-          truth = true;
-          for (j = 0, len = rolesToCheck.length; j < len; j++) {
-            r = rolesToCheck[j];
-            truth = truth && req.user.hasRole(r);
-          }
-          if (truth) {
-            return next();
+          req.user.removeRole = function(role) {
+            var getKey, i, key, keys, root;
+            getKey = function(root, key) {
+              return root[key];
+            };
+            keys = role.split(/\./g);
+            if (req.user.roles && keys.length) {
+              root = req.user.roles;
+              i = 0;
+              key = '';
+              while (i < keys.length - 1) {
+                key = keys[i];
+                root = getKey(root, key);
+                i++;
+              }
+              key = keys[keys.length - 1];
+              if (root[key]) {
+                delete root[key];
+              }
+            }
+            return ndx.database.exec('UPDATE ' + ndx.settings.USER_TABLE + ' SET roles=? where _id=?', [req.user.roles, req.user._id]);
+          };
+          req.user.hasRole = function(role) {
+            var allgood, getKey, j, key, keys, len, root;
+            getKey = function(root, key) {
+              return root[key];
+            };
+            keys = role.split(/\./g);
+            allgood = false;
+            if (req.user.roles) {
+              root = req.user.roles;
+              for (j = 0, len = keys.length; j < len; j++) {
+                key = keys[j];
+                root = getKey(root, key);
+                if (root) {
+                  allgood = true;
+                } else {
+                  allgood = false;
+                  break;
+                }
+              }
+            }
+            return allgood;
+          };
+        }
+        next();
+      }
+      return ndx.authenticate = function(role) {
+        return function(req, res, next) {
+          var getRole, j, len, r, rolesToCheck, truth;
+          if (req.user) {
+            rolesToCheck = [];
+            getRole = function(role) {
+              var j, len, r, results, type;
+              type = Object.prototype.toString.call(role);
+              if (type === '[object Array]') {
+                results = [];
+                for (j = 0, len = role.length; j < len; j++) {
+                  r = role[j];
+                  results.push(getRole(r));
+                }
+                return results;
+              } else if (type === '[object Function]') {
+                r = role(req);
+                return getRole(r);
+              } else if (type === '[object String]') {
+                if (rolesToCheck.indexOf(role) === -1) {
+                  return rolesToCheck.push(role);
+                }
+              }
+            };
+            getRole(role);
+            truth = true;
+            for (j = 0, len = rolesToCheck.length; j < len; j++) {
+              r = rolesToCheck[j];
+              truth = truth && req.user.hasRole(r);
+            }
+            if (truth) {
+              return next();
+            } else {
+              throw ndx.UNAUTHORIZED;
+            }
           } else {
             throw ndx.UNAUTHORIZED;
           }
-        } else {
-          throw ndx.UNAUTHORIZED;
-        }
+        };
       };
-    };
+    });
   };
 
 }).call(this);
--- src/app.coffee
+++ src/app.coffee
@@ -2,81 +2,80 @@
 
 module.exports = (ndx) ->
   ndx.app.use '/api/*', (req, res, next) ->
-    if not req.user
-      req.user = {}
-    if Object.prototype.toString.call(req.user) is '[object Object]'
-      req.user.addRole = (role) ->
-        addKey = (root, key) ->
-          if not root[key]
-            root[key] = {}
-          root[key]
-        keys = role.split /\./g
-        if not req.user.roles
-          req.user.roles = {}
-        root = req.user.roles
-        for key in keys
-          root = addKey root, key
-        ndx.database.exec 'UPDATE ' + ndx.settings.USER_TABLE + ' SET roles=? where _id=?', [
-          req.user.roles
-          req.user._id
-        ]
-      req.user.removeRole = (role) ->
-        getKey = (root, key) ->
-          root[key]
-        keys = role.split /\./g
-        if req.user.roles and keys.length
-          root = req.user.roles
-          i = 0
-          key = ''
-          while i < keys.length - 1
-            key = keys[i]
-            root = getKey root, key
-            i++
-          key = keys[keys.length - 1]
-          if root[key]
-            delete root[key]
-        ndx.database.exec 'UPDATE ' + ndx.settings.USER_TABLE + ' SET roles=? where _id=?', [
-          req.user.roles
-          req.user._id
-        ]
-      req.user.hasRole = (role) ->
-        getKey = (root, key) ->
-          root[key]
-        keys = role.split /\./g
-        allgood = false
-        if req.user.roles
+    if req.user
+      if Object.prototype.toString.call(req.user) is '[object Object]'
+        req.user.addRole = (role) ->
+          addKey = (root, key) ->
+            if not root[key]
+              root[key] = {}
+            root[key]
+          keys = role.split /\./g
+          if not req.user.roles
+            req.user.roles = {}
           root = req.user.roles
           for key in keys
-            root = getKey root, key
-            if root
-              allgood = true
-            else
-              allgood = false
-              break
-        allgood
-    next()
-  ndx.authenticate = (role) ->
-    (req, res, next) ->
-      if req.user
-        rolesToCheck = []
-        getRole = (role) ->
-          type = Object.prototype.toString.call role
-          if type is '[object Array]'
-            for r in role
+            root = addKey root, key
+          ndx.database.exec 'UPDATE ' + ndx.settings.USER_TABLE + ' SET roles=? where _id=?', [
+            req.user.roles
+            req.user._id
+          ]
+        req.user.removeRole = (role) ->
+          getKey = (root, key) ->
+            root[key]
+          keys = role.split /\./g
+          if req.user.roles and keys.length
+            root = req.user.roles
+            i = 0
+            key = ''
+            while i < keys.length - 1
+              key = keys[i]
+              root = getKey root, key
+              i++
+            key = keys[keys.length - 1]
+            if root[key]
+              delete root[key]
+          ndx.database.exec 'UPDATE ' + ndx.settings.USER_TABLE + ' SET roles=? where _id=?', [
+            req.user.roles
+            req.user._id
+          ]
+        req.user.hasRole = (role) ->
+          getKey = (root, key) ->
+            root[key]
+          keys = role.split /\./g
+          allgood = false
+          if req.user.roles
+            root = req.user.roles
+            for key in keys
+              root = getKey root, key
+              if root
+                allgood = true
+              else
+                allgood = false
+                break
+          allgood
+      next()
+    ndx.authenticate = (role) ->
+      (req, res, next) ->
+        if req.user
+          rolesToCheck = []
+          getRole = (role) ->
+            type = Object.prototype.toString.call role
+            if type is '[object Array]'
+              for r in role
+                getRole r
+            else if type is '[object Function]'
+              r = role req
               getRole r
-          else if type is '[object Function]'
-            r = role req
-            getRole r
-          else if type is '[object String]'
-            if rolesToCheck.indexOf(role) is -1
-              rolesToCheck.push role
-        getRole role
-        truth = true
-        for r in rolesToCheck
-          truth = truth and req.user.hasRole(r)
-        if truth
-          next()
+            else if type is '[object String]'
+              if rolesToCheck.indexOf(role) is -1
+                rolesToCheck.push role
+          getRole role
+          truth = true
+          for r in rolesToCheck
+            truth = truth and req.user.hasRole(r)
+          if truth
+            next()
+          else
+            throw ndx.UNAUTHORIZED
         else
           throw ndx.UNAUTHORIZED
-      else
-        throw ndx.UNAUTHORIZED
